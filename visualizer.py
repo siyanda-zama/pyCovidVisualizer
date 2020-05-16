da2686332fc117ffa181e5870699c31d008556d6
Siyanda Zama <54079671+siyanda-zama@users.noreply.github.com>
2020-05-16 10:55:28 +0200

    Second commit

--- visualizer.py
+++ visualizer.py
@@ -1,24 +1,3 @@
-# import ssl
-# ssl._create_default_https_context = ssl._create_unverified_context
-#
-# import matplotlib.pyplot as plt
-# import pandas as pd
-#
-# URL_DATASET = r'https://raw.githubusercontent.com/datasets/covid-19/master/data/countries-aggregated.csv'
-# df1 = pd.read_csv(URL_DATASET)
-#
-#
-# sa = df1[df1['Country'] == 'South Africa']
-# print(sa.head(3))
-#
-#
-# plt.rcParams["figure.figsize"]=20,20
-#
-# sa.plot(kind = 'bar', x = 'Date', y = 'Confirmed', color = 'blue')
-#
-# ax1 = plt.gca()
-# sa.plot(kind = 'bar', x = 'Date', y = 'Deaths', color = 'red', ax = ax1)
-# plt.show()
 
 
 import ssl
